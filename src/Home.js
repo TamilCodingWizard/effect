c64a3af03a50d8af32592a080c30539e5e822ab5
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-08-18 22:59:01 +0100

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,31 @@
-
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
-  const [counter1, setCounter1] = useState(0)
-  const [counter2, setCounter2] = useState(0)
+  // // Type 1: Executes on render and all state changes
+  // useEffect(() => {
+  //   console.log('Use Effect executing!')
+  // })
 
-//   // 1 Case - All dependency
-//   useEffect(() => {
-//     console.log("Screen refreshed")
-//   })
+  // // Type 2: Executes on render and all dependency state changes
+  // useEffect(() => {
+  //   console.log("Use Effect executing!");
+  // }, [counter2]);
 
-//   // 2nd Case -  dependency
-//   useEffect(() => {
-//     console.log("Screen refreshed")
-//   },[counter2,counter1])
-
-  // 3rd Case -  empty array
+  // Type 3: Executes on render 
   useEffect(() => {
-    console.log("Screen refreshed")
-  },[])
+    console.log("Use Effect executing!");
+  }, []);
 
   return (
     <>
-        <h1>{counter1}</h1>
-        <h1>{counter2}</h1>
+      <h1>{counter1}</h1>
+      <h1>{counter2}</h1>
 
-        <button onClick={() => setCounter1(counter1 + 1)}>Increment 1</button>
-        <button onClick={() => setCounter2(counter2 + 1)}>Increment 2</button>
-
+      <button onClick={() => setCounter1(counter1 + 1)}>Increment 1</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>Increment 2</button>
     </>
-  )
+  );
 }
